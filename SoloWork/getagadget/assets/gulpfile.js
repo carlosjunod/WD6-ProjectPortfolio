876a4416b55a5d62b0bb19e2d1966ae11c0b4a15
carlosjunod <carlos@junod.cl>
2017-07-22 20:00:57 -0400

    initial setup done

---
+++ SoloWork/getagadget/assets/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+// const babel = require('gulp-babel');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+var sassPaths = [
+  'bower_components/normalize.scss/sass',
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        proxy: "localhost:3100"
+    })
+    console.log('reload in browserSync');
+})
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(browserSync.reload({
+        stream:true
+    }))
+    .pipe(gulp.dest('../public/stylesheets'));
+
+
+});
+
+gulp.task('watch', ['sass','browser-sync'], function(){
+    gulp.watch(['./scss/**/*.scss'], ['sass']).on('change', reload)
+    gulp.watch(['../views/*.pug']).on('change', reload)
+
+
+})
