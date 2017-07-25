7c0a57549d7ef9aee0cc7b5c8bc3f81690458444
carlosjunod <carlos@junod.cl>
2017-07-24 22:15:22 -0400

    Seeding done, displaying at index

--- SoloWork/getagadget/routes/index.js
+++ SoloWork/getagadget/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  var products = Product.find((err, docs)=>{
+    console.log(err);
+    res.render('shop/index', { title: 'Express', products: docs });
+  });
+
+
 });
 
 module.exports = router;
